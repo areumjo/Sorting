3de291f03388e1a31a3c42d92247e8ea5e87948b
Areum Jo <areumjo1@gmail.com>
2020-04-16 15:13:43 -0700

    Added sorting

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -1,4 +1,14 @@
 # TO-DO: Complete the selection_sort() function below 
+"""
+1. Start with current index = 0
+2. For all indices EXCEPT the last index:
+    - len(n-1) bc after the sort, the one remaining will be the largest
+    a. Loop through elements on right-hand-side of current index and find the smallest element
+    b. Swap the element at current index with the smallest element found in above loop
+"""
+
+import math
+
 def selection_sort( arr ):
     # loop through n-1 elements
     for i in range(0, len(arr) - 1):
@@ -6,25 +16,72 @@ def selection_sort( arr ):
         smallest_index = cur_index
         # TO-DO: find next smallest element
         # (hint, can do in 3 loc) 
-             
-
-
-
+        for j in range(cur_index+1, len(arr)):
+            if arr[j] < arr[smallest_index]:
+                smallest_index = j
         # TO-DO: swap
-
-
-
-
+        arr[smallest_index], arr[cur_index] = arr[cur_index], arr[smallest_index] 
     return arr
 
 
 # TO-DO:  implement the Bubble Sort function below
+"""
+1. Loop through your array
+    - Compare each element to its neighbor
+    - If elements in wrong position (relative to each other, swap them)
+2. If no swaps performed, stop. Else, go back to the element at index 0 and repeat step 1.
+"""
 def bubble_sort( arr ):
-
+    for i in range(0, len(arr)):
+        for j in range(0, len(arr)-i-1):
+            if arr[j] > arr[j+1]:
+                # print(f'i: {i}, j: {j}, {arr}')
+                temp = arr[j]
+                arr[j] = arr[j+1]
+                arr[j+1] = temp
     return arr
 
 
 # STRETCH: implement the Count Sort function below
+"""
+Count sort : based on keys between a specific range
+    - linear time : O(n) // faster 
+    - restricted inputs : only works when the range of items in the input is know ahead of time
+    - space cose : O(n)
+"""
 def count_sort( arr, maximum=-1 ):
+    # count the number of times each value appears
+    counts = [0] * (maximum + 1) # n=5 [0, 0, 0, 0, 0, 0]
+    for item in arr:
+        counts[item] += 1
+    total = 0
+    for i in range(1, len(counts)):
+        counts[i], total = total, counts[i] + total
+    sorted_list = [None] * len(arr)
+    for item in arr:
+        sorted_list[counts[item]] = item
+        counts[item] += 1
+    return arr
 
+
+# insertion sort
+"""
+Time complexity : O(n^2) 
+    - "in place" (not using extra memory || not creating new array)
+Space complexity : O(1)
+"""
+def insertion_sort( arr ):
+    # Divide your hand into sorted on the left and unsorted on the right
+    # arr[0] = sorted || start arr[1] unsorted
+    # go card by card and move them into place
+    # loop through all elements in unsorted
+    for i in range(1, len(arr)):
+        temp = arr[i]
+        j = i # j == sliding index
+        print(f'i is {i}, j = {j} temp = {temp}')
+        # shift sorted to the right until correct position found
+        while j > 0 and temp < arr[j - 1]:
+            arr[j] = arr[j - 1] # slide over one element
+            j -= 1
+            arr[j] = temp
     return arr
--- src/recursive_sorting/recursive_sorting.py
+++ src/recursive_sorting/recursive_sorting.py
@@ -3,14 +3,43 @@ def merge( arrA, arrB ):
     elements = len( arrA ) + len( arrB )
     merged_arr = [0] * elements
     # TO-DO
-    
+    a_i = 0
+    b_i = 0
+    for i in range(elements):
+        # print(f'a,b: {a_i}, {b_i}, {merged_arr}')
+        if a_i >= len(arrA):
+            merged_arr[i] = arrB[b_i]
+            b_i += 1
+        elif b_i >= len(arrB):
+            merged_arr[i] = arrA[a_i]
+            a_i += 1
+        elif arrA[a_i] < arrB[b_i]:
+            merged_arr[i] = arrA[a_i]
+            a_i += 1
+        else:
+            merged_arr[i] = arrB[b_i]
+            b_i += 1
     return merged_arr
 
 
 # TO-DO: implement the Merge Sort function below USING RECURSION
+"""
+# (base case) if the array is empty or len(arr) == 1, return
+# split the arrays into half ==> arr[1], arr[2]
+# sort each half ==> recursively!
+# merge them back together : compare the first values fo each array, add smaller to result
+"""
+
 def merge_sort( arr ):
     # TO-DO
-
+    if len(arr) > 1:
+        #split
+        mid = len(arr) // 2
+        small = arr[:mid]
+        large = arr[mid:]
+        left = merge_sort(small)
+        right = merge_sort(large)
+        arr = merge(left, right)
     return arr
 
 
@@ -31,3 +60,20 @@ def merge_sort_in_place(arr, l, r):
 def timsort( arr ):
 
     return arr
+
+
+def quicksort(arr):
+    # base case
+    if len(arr) <= 1:
+        return arr
+    pivot = arr[0]
+    smaller = []
+    larger = []
+    for i in range(1, len(arr)):
+        if arr[i] <= pivot:
+            smaller.append(arr[i])
+        else:
+            larger.append(arr[i])
+    smaller = quicksort(smaller)
+    larger = quicksort(larger)
+    return smaller + [pivot] + larger
